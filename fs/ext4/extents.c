3098795d84e5a404a4d534a00de21a0e3a8acffb
rsuntk <rissu.ntk@gmail.com>
2025-09-11 06:51:56 +0000

    Revert "fs: ext4: Update from CIP"
    This reverts commit 485e661ac74d64b25e44963b5d3d3f12dbc90ff6.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -2562,19 +2562,18 @@ int ext4_ext_calc_credits_for_single_extent(struct inode *inode, int nrblocks,
 int ext4_ext_index_trans_blocks(struct inode *inode, int extents)
 {
 	int index;
+	int depth;
 
 	/* If we are converting the inline data, only one is needed here. */
 	if (ext4_has_inline_data(inode))
 		return 1;
 
-	/*
-	 * Extent tree can change between the time we estimate credits and
-	 * the time we actually modify the tree. Assume the worst case.
-	 */
+	depth = ext_depth(inode);
+
 	if (extents <= 1)
-		index = EXT4_MAX_EXTENT_DEPTH * 2;
+		index = depth * 2;
 	else
-		index = EXT4_MAX_EXTENT_DEPTH * 3;
+		index = depth * 3;
 
 	return index;
 }
